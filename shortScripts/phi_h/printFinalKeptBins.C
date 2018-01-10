a343442957554c3350697bdcec2a0c55cbd8f104
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2018-01-10 14:56:22 -0500

    Removed bins with huge error bars (A0 term only)

--- shortScripts/phi_h/printFinalKeptBins.C
+++ shortScripts/phi_h/printFinalKeptBins.C
@@ -16,7 +16,15 @@ if(!(xBin == 4 && QQBin == 1)) {
 	if(sysFile->GetListOfKeys()->Contains(pipSysHistName.c_str())) {
 	
 		if(MAcAccFile->GetListOfKeys()->Contains(pipMhistoName.c_str())) {
-			cout<<"pip_M_"<<xBin<<"_"<<QQBin<<"_"<<zBin<<"_"<<PT2Bin<<endl;
+			// for M, need to make sure error bars aren't out of control
+			TH1F *hsys = (TH1F*) sysFile->Get(pipSysHistName.c_str());
+			TH1F *hM = (TH1F*) MAcAccFile->Get(pipMhistoName.c_str());
+			float M_Esys = hsys->GetBinContent(1);
+			float M = hM->GetBinContent(1);
+			float M_Estat = hM->GetBinError(1);
+			if(M > M_Esys && M > M_Estat) {
+				cout<<"pip_M_"<<xBin<<"_"<<QQBin<<"_"<<zBin<<"_"<<PT2Bin<<endl;
+			}
 		}
 
 		if(MAcAccFile->GetListOfKeys()->Contains(pipAcAcchistoName.c_str())) {
@@ -24,6 +32,7 @@ if(!(xBin == 4 && QQBin == 1)) {
 		}
 	
 	}
+
 
 	// pim:
 	string pimSysHistName = Form("hMAcAccSys_pim_%i_%i_%i_%i", xBin, QQBin, zBin, PT2Bin);
@@ -33,7 +42,15 @@ if(!(xBin == 4 && QQBin == 1)) {
 	if(sysFile->GetListOfKeys()->Contains(pimSysHistName.c_str())) {
 	
 		if(MAcAccFile->GetListOfKeys()->Contains(pimMhistoName.c_str())) {
-			cout<<"pim_M_"<<xBin<<"_"<<QQBin<<"_"<<zBin<<"_"<<PT2Bin<<endl;
+			// for M, need to make sure error bars aren't out of control
+			TH1F *hsys = (TH1F*) sysFile->Get(pimSysHistName.c_str());
+			TH1F *hM = (TH1F*) MAcAccFile->Get(pimMhistoName.c_str());
+			float M_Esys = hsys->GetBinContent(1);
+			float M = hM->GetBinContent(1);
+			float M_Estat = hM->GetBinError(1);
+			if(M > M_Esys && M > M_Estat) {
+				cout<<"pim_M_"<<xBin<<"_"<<QQBin<<"_"<<zBin<<"_"<<PT2Bin<<endl;
+			}
 		}
 
 		if(MAcAccFile->GetListOfKeys()->Contains(pimAcAcchistoName.c_str())) {
